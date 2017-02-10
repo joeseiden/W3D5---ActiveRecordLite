cbbd1375d22faa601dc11102de859923dd3383b1
joeseiden <joe.seiden214@gmail.com>
2017-02-10 15:01:17 -0800

    Finish phase1

--- Active Record Lite/lib/01_sql_object.rb
+++ Active Record Lite/lib/01_sql_object.rb
@@ -86,17 +86,36 @@ class SQLObject
   end
 
   def attribute_values
+    self.class.columns.map { |attr_name| send(attr_name.to_sym) }
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(", ")
+    question_marks = ( ["?"] * self.class.columns.length ).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_line = self.class.columns
+                .map { |attr_name| "#{attr_name} = ?" }.join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values, self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    self.id.nil? ? self.insert : self.update
   end
 end
